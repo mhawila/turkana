7d5fac70084f24270179a992e03243db0a12ac5e
samuelowino <samuelowino43@yahoo.com>
2019-09-19 11:07:10 +0300

    Fcm Push Notifications Infra Setup

--- src/main/java/org/muzima/TurkanaApplication.java
+++ src/main/java/org/muzima/TurkanaApplication.java
@@ -9,5 +9,4 @@ public class TurkanaApplication {
     public static void main(String[] args) {
         SpringApplication.run(TurkanaApplication.class, args);
     }
-
 }
---
+++ src/main/java/org/muzima/turkana/service/PushNotificationService.java
@@ -0,0 +1,77 @@
+package org.muzima.turkana.service;
+
+import org.muzima.turkana.model.PushNotificationRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class PushNotificationService {
+
+    @Value("#{${application.properties}}")
+    private Map<String, String> defaults;
+
+    private Logger logger = LoggerFactory.getLogger(PushNotificationService.class);
+
+    private FcmService fcmService;
+
+    public PushNotificationService(FcmService fcmService) {
+        this.fcmService = fcmService;
+    }
+
+    @Scheduled(initialDelay = 60000, fixedDelay = 60000)
+    public void sendSamplePushNotification() {
+        try {
+            fcmService.sendMessageWithoutData(getSamplePushNotificationRequest());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendPushNotification(PushNotificationRequest request) {
+        try {
+            fcmService.sendMessage(getSamplePayloadData(), request);
+        } catch (InterruptedException | ExecutionException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void sendPushNotificationWithoutData(PushNotificationRequest request) {
+        try {
+            fcmService.sendMessageWithoutData(request);
+        } catch (InterruptedException | ExecutionException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void sendPushNotificationToToken(PushNotificationRequest request) {
+        try {
+            fcmService.sendMessageToToken(request);
+        } catch (InterruptedException | ExecutionException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private Map<String, String> getSamplePayloadData() {
+        Map<String, String> pushData = new HashMap<>();
+        pushData.put("messageId", defaults.get("payloadMessageId"));
+        pushData.put("text", defaults.get("payloadData") + " " + LocalDateTime.now());
+        return pushData;
+    }
+
+    private PushNotificationRequest getSamplePushNotificationRequest() {
+        PushNotificationRequest request = new PushNotificationRequest(defaults.get("title"),
+            defaults.get("message"),
+            defaults.get("topic"));
+        return request;
+    }
+}
